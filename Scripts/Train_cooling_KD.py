e3760affc928c78562dd9a808c11e04bb3cdd12a
Xiangrui Liu <35613337+lxrswdd@users.noreply.github.com>
2023-12-17 16:04:22 -0800

    Update Train_cooling_KD.py

--- Scripts/Train_cooling_KD.py
+++ Scripts/Train_cooling_KD.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 import mytools
-from YRQ_fsmn import CVFSMNv2
+from FSMN import CVFSMNv2
 import torch.nn.functional as F
 from collections import Counter
 import x_vector
